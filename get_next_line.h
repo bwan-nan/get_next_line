bc3b36eb8ec34532fe54bf2d3c50216da4db1e54
Benjamin WAN NAN WO <bwan-nan@e2r12p16.42.fr>
2018-11-21 15:56:32 +0100

    First commit

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwan-nan <bwan-nan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/21 11:59:01 by bwan-nan          #+#    #+#             */
+/*   Updated: 2018/11/21 15:52:53 by bwan-nan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# define BUFF_SIZE 1
+int		get_next_line(const int fd, char **line);
+#endif
