d33f712b905586a5142d16624e6a31dbe487adc2
Benjamin WAN NAN WO <bwan-nan@e2r12p16.42.fr>
2018-11-21 17:40:36 +0100

    random push

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,23 @@
 /*   By: bwan-nan <bwan-nan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 11:56:09 by bwan-nan          #+#    #+#             */
-/*   Updated: 2018/11/21 15:51:02 by bwan-nan         ###   ########.fr       */
+/*   Updated: 2018/11/21 17:24:42 by bwan-nan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <unistd.h>
 #include "get_next_line.h"
 
 int		get_next_line(const int fd, char **line)
 {
+	char buffer;
+	char *str;
+
+	while (read(fd, &buffer, 1))
+	{
+		*str = buffer;
+		ft_strjoin(*line, str);
+	}
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: bwan-nan <bwan-nan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 11:59:01 by bwan-nan          #+#    #+#             */
-/*   Updated: 2018/11/21 15:52:53 by bwan-nan         ###   ########.fr       */
+/*   Updated: 2018/11/21 16:21:35 by bwan-nan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
+# include "./libft/libft.h"
 # define BUFF_SIZE 1
 int		get_next_line(const int fd, char **line);
 #endif
